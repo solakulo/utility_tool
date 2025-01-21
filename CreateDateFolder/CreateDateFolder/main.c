2930332668ebf226bed10a529e9e3ab04eaaff5e
solakulo <729861921cp@gmail.com>
2025-01-21 11:30:17 +0900

    V0.01 Add main.c
    - Created main.c, which uses the system time to generate a folder name based on the current date.
    - The folder is created in the current directory.
    - This commit is part of the initial setup and basic functionality.

---
+++ CreateDateFolder/CreateDateFolder/main.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <time.h>
+#include <direct.h> // For _mkdir on Windows
+
+int main() {
+    // Get the current date
+    time_t t = time(NULL);
+    struct tm* tm_info = localtime(&t);
+    char folder_name[20];
+    strftime(folder_name, sizeof(folder_name), "%Y-%m-%d", tm_info);
+
+    // Create the folder
+    if (_mkdir(folder_name) == 0) {
+        printf("Folder '%s' created successfully!\\n", folder_name);
+    }
+    else {
+        printf("Failed to create folder '%s'. It may already exist.\\n", folder_name);
+    }
+
+    return 0;
+}
